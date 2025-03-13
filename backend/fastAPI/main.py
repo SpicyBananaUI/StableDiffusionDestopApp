6c4765c7af3ffee9ec831b6663e241661dba8724
ronan-morris <ronanm@tuta.io>
2025-03-13 09:51:03 -0400

    added metal support to Zakhar's mini FastApi

--- backend/fastAPI/main.py
+++ backend/fastAPI/main.py
@@ -4,7 +4,25 @@ from diffusers import StableDiffusionPipeline
 app = FastAPI()
 
 model_id = "CompVis/stable-diffusion-v1-4"
-device = "cpu"
+
+if torch.cuda.is_available():
+    print("CUDA is available.")
+    device = torch.device("cuda")
+    print("Device name:", torch.cuda.get_device_name(0))
+    print("CUDA version:", torch.version.cuda)
+    print("CUDNN version:", torch.backends.cudnn.version())
+elif torch.backends.mps.is_available():
+    print("MPS (Metal Performance Shaders) is available.")
+    device = torch.device("mps")
+    mps_device = torch.device("mps")
+    print("MPS device:", mps_device)
+    print("PyTorch MPS backend is enabled:", torch.backends.mps.is_built())
+else:
+    print("No GPU acceleration available. Using CPU.")
+    device = torch.device("cpu")
+
+print("Using device:", device)
+
 pipe = StableDiffusionPipeline.from_pretrained(model_id)
 pipe = pipe.to(device)
 
